4a527305ab487605a8b00f59d5e10efa601b3435
francoisquellec <francois.quellec@heig-vd.ch>
2018-04-12 15:48:47 +0200

    almost done

--- Labo3/map_generator.py
+++ Labo3/map_generator.py
@@ -11,61 +11,31 @@ import math
 import os
 import numpy as np
 
-# Check if the argument is an Integer
-def isInteger(char):
-    try: 
-        int(char)
-        return True
-    except ValueError:
-        return False
-
-# Parse the input into an array with the right dimensions
-def parseInput(filename):
-    inputFile = open(filename, "r")
-    
-    dimensions = inputFile.readline().split(' ')
-    width = int(dimensions[0])
-    height = int(dimensions[1])
-
-    inputTab = np.empty((height, width), 'int')
-
-    for y in range(0, height):
-        dephts = inputFile.readline().split(' ')
-        for x in range(0, width):
-            if isInteger(dephts[x]):
-                inputTab[y][x] = dephts[x]
-
-    inputFile.close() 
-    return inputTab
-
-#Initialise the map with points
-def initPoints(inputTab):
-    vtkPoints = vtk.vtkPoints()
-    for y in range(0,len(inputTab)):
-        for x in range(0,len(inputTab[0])):
-            vtkPoints.InsertNextPoint(x, y, inputTab[y][x])
-          
-    return vtkPoints
-
-
 def main():
     # Get the solutions
-    inputTab = parseInput("altitudes.txt")
-    vtkPoints = initPoints(inputTab)
+    file = open("altitudes.txt", "r")
+    
+    xSize, ySize = [int(i) for i in file.readline().split()]
 
-    # Add the grid points to a polydata object
-    inPolyData = vtk.vtkPolyData()
-    inPolyData.SetPoints(vtkPoints)
+    # creating geometry
+    points = vtk.vtkPoints()
+    altitudes = vtk.vtkIntArray()
+    for x in range(xSize):
+        altitude = file.readline().split()
+        for y in range(ySize):
+            points.InsertNextPoint(x, y, int(altitude[y])/4)
+            altitudes.InsertNextValue(int(altitude[y]))
+    file.close() 
 
-    # Triangulate the grid points
-    delaunay = vtk.vtkDelaunay2D()
-    delaunay.SetInputData(inPolyData);
-    delaunay.Update()
-    outPolyData = delaunay.GetOutput()
+    # creating dataset with implicit topology
+    grid = vtk.vtkStructuredGrid()
+    grid.SetDimensions(xSize, ySize, 1)
+    grid.SetPoints(points)
+    grid.GetPointData().SetScalars(altitudes)
 
     # Find min and max z
     bounds = [0]*6
-    outPolyData.GetBounds(bounds)
+    grid.GetBounds(bounds)
     minz = bounds[4]
     maxz = bounds[5]
 
@@ -74,35 +44,11 @@ def main():
     colorTable.SetTableRange(minz, maxz)
     colorTable.Build()
 
-    # Init our colors array
-    colors = vtk.vtkUnsignedCharArray()
-    colors.SetNumberOfComponents(3)
-    colors.SetName("Colors")
-
-    # For each point find the color with the LUT table
-    for i in range(0, outPolyData.GetNumberOfPoints()):
-        # Get the next point coordinates
-        point = [0,0,0]
-        outPolyData.GetPoint(i, point)
-
-        # Get the associate color with the LUT
-        dcolor = [0, 0, 0]
-        colorTable.GetColor(point[2], dcolor);
-
-        # Convert in the right format
-        color = [0, 0, 0]
-        for x in range(0,3):
-            color[x] = dcolor[x] * 255
-
-        # Add the color to the scalar
-        colors.InsertNextTuple3(color[0], color[1], color[2])
-
-    # Assign the colors to the polydata's points 
-    outPolyData.GetPointData().SetScalars(colors)
-
     # Create a mapper and actor
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputData(outPolyData)
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(grid)
+    mapper.UseLookupTableScalarRangeOn()
+    mapper.SetLookupTable(colorTable)
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
 
