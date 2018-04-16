197555187f1794b8650ee60cc961ea164439bdd3
francoisquellec <francois.quellec@heig-vd.ch>
2018-04-16 11:21:45 +0200

    spherical Transform using Rotate and Translate ok

--- Labo3/map_generator.py
+++ Labo3/map_generator.py
@@ -14,24 +14,52 @@ import numpy as np
 def main():
     # Get the solutions
     file = open("altitudes.txt", "r")
-    
+
     xSize, ySize = [int(i) for i in file.readline().split()]
     radius = 6371009
     phi_min = 45
     phi_max = 47.5
     theta_min = 5
     theta_max = 7.5
+    delta_phi = (phi_max - phi_min)/xSize
+    delta_theta = (theta_max - theta_min)/ySize
 
     # creating geometry
     points = vtk.vtkPoints()
     altitudes = vtk.vtkIntArray()
+    minz = radius * 2
+    maxz = -1
 
     for x in range(xSize):
         altitude = file.readline().split()
         for y in range(ySize):
-            z = int(altitude[y])
-            points.InsertNextPoint(x, y, z)
+            #print("RotateX:", delta_phi*x + phi_min)
+            #print("RotateY:", delta_theta*y + theta_min)
+            z = radius + int(altitude[y])
+
+            t = vtk.vtkTransform()
+
+            #Place the point  
+            t.RotateX(delta_phi*x + phi_min)
+            t.RotateY(delta_theta*y + theta_min)
+            t.Translate(0, 0, z)
+            
+            #Apply transformation
+            p_in = [0, 0, 0, 1]
+            p_out = [0, 0, 0, 1]
+            t.MultiplyPoint(p_in, p_out)
+
+            #print("p:", p_out[0], p_out[1], p_out[2])
+            points.InsertNextPoint(p_out[0], p_out[1], p_out[2])
+
             altitudes.InsertNextValue(z)
+            if(z < minz):
+                minz = z
+            if(z > maxz):
+                maxz = z
+
+            
+
     file.close() 
 
     # creating dataset with implicit topology
@@ -39,12 +67,6 @@ def main():
     grid.SetDimensions(xSize, ySize, 1)
     grid.SetPoints(points)
     grid.GetPointData().SetScalars(altitudes)
-
-    # Find min and max z
-    bounds = [0]*6
-    grid.GetBounds(bounds)
-    minz = bounds[4]
-    maxz = bounds[5]
 
     # Init our LUT table to find the colors
     colorTable = vtk.vtkLookupTable()
@@ -58,9 +80,6 @@ def main():
     mapper.SetLookupTable(colorTable)
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
-
-    t = vtk.vtkSphericalTransform()
-    actor.SetUserTransform(t)
 
     # Create the renderer 
     ren1= vtk.vtkRenderer()
