e3524b0e944927cbc29571c50d8f1d277094a9ad
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-05-08 18:36:57 +0200

    start clipping with sphere

--- Labo4/knee.py
+++ Labo4/knee.py
@@ -6,8 +6,7 @@
 #############################################
 
 import vtk
-import antigravity
-
+import math
 
 def load_image_data():
     # Loading image data from file
@@ -16,23 +15,96 @@ def load_image_data():
     reader.Update()
     return reader
 
+
+def define_viewports(renderers):
+    # Number of column to display
+    col = 2
+    row = math.ceil(len(renderers)/col)
+    colSize = 1 / col
+    rowSize = 1 / row
+
+    # Steps viewports
+    for index in range(len(renderers)):
+        i = index % col
+        j = math.floor(index/col)
+
+        ren = renderers[index]
+
+        if index == len(renderers) - 1:  # last renderer
+            ren.SetViewport(i * colSize, 1 - rowSize - j * rowSize, 1, rowSize)
+        else:
+            ren.SetViewport(i * colSize, 1 - rowSize - j *
+                            rowSize, (i + 1) * colSize, 1 - j * rowSize)
+
+def create_iso_dataset(input, iso_value):
+    contour = vtk.vtkContourFilter()
+    contour.SetInputConnection(input.GetOutputPort())
+    contour.SetValue(0, iso_value)
+
+    return contour
+
+def create_iso_actor(input, iso_value):
+    contour = create_iso_dataset(input, iso_value)
+    return create_actor(contour)
+
+def create_mapper(input):
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputConnection(skin.GetOutputPort())
+    mapper.ScalarVisibilityOff()
+
+    return mapper
+
+
+def create_bone(image_data):
+    return create_iso_dataset(image_data, 73)
+
+def create_skin(image_data):
+    return create_iso_dataset(image_data, 50)
+
+
+def create_2(bone, skin):
+    # creating the sphere clipping
+    sphere = vtk.vtkSphere()
+    sphere.SetRadius(60)
+    sphere.SetCenter(0, 100, 100)
+
+    # clipping
+    clipper = vtk.vtkClipDataSet()
+    clipper.SetClipFunction(sphere)
+    clipper.SetInputConnection(skin.GetOutputPort())
+    clipper.SetValue(0)
+    clipper.Update()
+
+    skin = clipper
+
+    return (bone, skin)
+
 def main():
     image_data = load_image_data()
 
-    mapper = vtk.vtkDataSetMapper()
-    mapper.SetInputData(image_data.GetOutput())
+    bone = create_bone(image_data)
+    skin = create_skin(image_data) 
 
+    val = create_2(bone, skin)
+        
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputConnection(val[1].GetOutputPort())
+    mapper.ScalarVisibilityOff()
+    
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
-    actor.GetProperty().SetRepresentationToWireframe()
 
     # ------------ RENDERING -------------
-    # Creating a render
+    # Creating renderers
     ren = vtk.vtkRenderer()
     ren.AddActor(actor)
     ren.SetBackground(1, 1, 1)
+    renderers = [ren]
 
-    # Creating a window to display the render
+    # Creating viewports
+    viewports = define_viewports(renderers)
+
+    # Creating a window to display the viewports
     renWin = vtk.vtkRenderWindow()
     renWin.AddRenderer(ren)
     renWin.SetSize(900, 900)
